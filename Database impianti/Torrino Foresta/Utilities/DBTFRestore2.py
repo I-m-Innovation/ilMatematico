8d2d683b9c7823edaee9b9762776f1d8f5ea39e5
stefanoZS <stefano.trevisan@zilioservice.com>
2024-06-17 11:29:22 +0200

    Sistemato DB Torrino Foresta, aumentato il dt per l'allarme

---
+++ Database impianti/Torrino Foresta/Utilities/DBTFRestore2.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("DBTFNEW3.csv")
+
+PLC1_AI_PT_TURBINA = df["PLC1_AI_PT_TURBINA"]
+
+TEST = PLC1_AI_PT_TURBINA
+# TEST = float(PLC1_AI_PT_TURBINA.replace(",", "."))
+
+TEST_REV = TEST
+TEST_REV[TEST>4] = TEST[TEST > 4]/10.1974
+
+PLC1_AI_PT_TURBINA = TEST_REV
+
+dfOut = df
+dfOut["PLC1_AI_PT_TURBINA"] = PLC1_AI_PT_TURBINA
+dfOut.to_csv("DBTFNEW3.csv")
