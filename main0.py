2c237bd0a8a77d9d8b24865db4ef764d438160ad
Sviluppo_Software_ZG <stefano.trevisan@zilioenvironment.com>
2024-11-27 16:07:59 +0100

    modifiche db

--- main0.py
+++ main0.py
@@ -15,7 +15,7 @@ p = psutil.Process(os.getpid())
 
 
 def main(main_data, bot_data):
-
+    print("ok")
     print("Scansione degli impianti iniziata.")
     sa3_data_new = scan("SA3", main_data["SA3"], bot_data)
     zg_data_new = scan("ZG", main_data["ZG"], bot_data)
